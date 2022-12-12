58ba0d17fdd852a714d37f1e947121e9b41f7c0b
OliverPerezyavirac <you@example.com>
2022-12-11 19:03:49 -0500

    templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RoutingModule } from './appRouting.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './static/header/header.component';
+import { FooterComponent } from './static/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
